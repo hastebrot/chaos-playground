5029b8360f45faddc3f17826a83eb4a3cfbb02eb
Benjamin Gudehus <hastebrot@gmail.com>
2017-05-10 23:57:10 +0200

    (feat) Add simple canvas animation.

--- TODO.md
+++ TODO.md
@@ -7,9 +7,9 @@ TODO
     - langton ameise
     - game of life
     - regelsystem (prozedual, oder anders?)
+    - states
     
 - infrastructure:
     - frame
     - widgets (canvas, slider)
     - paint, animation
-
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,51 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 public class Main {
 
+    public static class FooApp extends Application {
+        private int x = 0;
+        private int y = 0;
+
+        @Override
+        public void start(Stage stage) throws Exception {
+            Canvas canvas = new Canvas(400, 400);
+            stage.setScene(new Scene(new StackPane(canvas), 420, 420));
+            stage.setOnShowing(windowEvent -> {
+                final KeyFrame keyFrame = new KeyFrame(Duration.millis(10), (event) -> {
+                    GraphicsContext context = canvas.getGraphicsContext2D();
+                    context.setFill(Color.BLACK);
+
+                    context.rect(x, y, 10, 10);
+                    context.fill();
+                    x += 10;
+                    if (x >= 400) {
+                        x = 0;
+                        y += 10;
+                    }
+                });
+
+                final Timeline timeline = new Timeline();
+                timeline.setCycleCount(Timeline.INDEFINITE);
+                timeline.setAutoReverse(true);
+                timeline.getKeyFrames().add(keyFrame);
+                timeline.play();
+
+            });
+            stage.show();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Application.launch(FooApp.class);
     }
 }
+
